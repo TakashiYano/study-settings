059882fd2f3c39134645c915a39a787495e7413a
yanot <0tp6324w3261z0v@gmail.com>
2022-06-10 16:04:37 +0900

    🔧 Eslintの設定をアップデート

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
   env: { es2021: true, browser: true, jest: true, node: true },
   plugins: ["simple-import-sort"],
   extends: [
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
     "next",
     "next/core-web-vitals",
     "prettier",
